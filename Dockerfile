09bbb0e160e93e9798b88b5747528b868da9aeb3
PranayB003 <pranayborgohain@gmail.com>
2023-01-04 16:07:01 +0530

    finished first task

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ RUN npm install
 
 EXPOSE 3000
 
+HEALTHCHECK --interval=30s --timeout=5s --start-period=5s \
+    CMD curl -f 127.0.0.1:3000/api/v1/healthcheck || exit 1
+
 COPY ./ ./
 
 CMD [ "node", "src/server.js" ]
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("./models/userModel");
 
 const notesRouter = require("./routes/notesRoutes");
 const authRouter = require("./routes/authRoutes");
@@ -6,6 +7,16 @@ const authRouter = require("./routes/authRoutes");
 const app = express();
 
 app.use(express.json());
+
+// Healthcheck endpoint
+app.use("/api/v1/healthcheck", async (req, res, next) => {
+    try {
+        await User.find();
+        res.sendStatus(200);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
 
 app.use("/api/v1/", authRouter);
 app.use("/api/v1/notes", notesRouter);
